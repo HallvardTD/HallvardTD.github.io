177f762eeec1355afbba7709e1139f157d8fe945
HalvorHetland <70580505+HalvorHetland@users.noreply.github.com>
2021-10-27 15:00:39 +0200

    Initial commit

---
+++ src/main/java/com/example/mousescrolltest/MouseScrollTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mousescrolltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouseScrollTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MouseScrollTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mousescrolltest/MouseScrollTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mousescrolltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MouseScrollTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
